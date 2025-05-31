d671ee4f60135e55fc4d17311b844db38598f27c
laochonlam <laochanlam@gmail.com>
2025-05-31 17:56:52 -0400

    check hw counter directly to avoid high ethtool overhead

--- main.py
+++ main.py
@@ -1,60 +1,56 @@
-import subprocess
 import time
 
 interval = 1.0
-last_index_total_tx1 = 0
-last_index_total_rx1 = 0
-last_index_total_tx2 = 0
-last_index_total_rx2 = 0
+last_tx1 = 0
+last_rx1 = 0
+last_tx2 = 0
+last_rx2 = 0
 
 init = False
 
+def read_counter(interface, counter):
+    with open(f'/sys/class/net/{interface}/statistics/{counter}', 'r') as f:
+        return int(f.read().strip())
+
 while True:
     print("####################################")
-    interface = 'enp178s0f0'
-    baescmd = ("sudo  ethtool -S ") + interface
+    interface = 'enp37s0f0np0'
+    
+    current_tx = read_counter(interface, 'tx_bytes')
+    current_rx = read_counter(interface, 'rx_bytes')
 
-    result = subprocess.check_output(baescmd, shell=True).split()
-    index_total_tx = result.index('rx_bytes_phy:') + 1
-    index_total_rx = result.index('tx_bytes_phy:') + 1
-
-    if (not init):
-        bandwidth_tx = (float(result[index_total_tx]) - float(result[index_total_tx])) / interval
-        bandwidth_rx = (float(result[index_total_rx]) - float(result[index_total_rx])) / interval
+    if not init:
+        bandwidth_tx = (current_tx - current_tx) / interval
+        bandwidth_rx = (current_rx - current_rx) / interval
     else:
-        bandwidth_tx = (float(result[index_total_tx]) - float(last_index_total_tx1)) / interval
-        bandwidth_rx = (float(result[index_total_rx]) - float(last_index_total_rx1)) / interval
+        bandwidth_tx = (current_tx - last_tx1) / interval
+        bandwidth_rx = (current_rx - last_rx1) / interval
 
     print(interface + ':')
-    # print('rx_bytes_phy: ' + str(result[index_total_tx]))
-    # print('tx_bytes_phy: ' + str(result[index_total_rx]))
-    print('Tx BW: ' + str(0.0 if bandwidth_tx < 200 else bandwidth_tx/1024.0/1024.0/1024.0*8) + ' Gbps')
-    print('Rx BW: ' + str(0.0 if bandwidth_rx < 200 else bandwidth_rx/1024.0/1024.0/1024.0*8) + ' Gbps')
+    print('Tx BW: ' + str(0.0 if bandwidth_tx < 200 else round(bandwidth_tx/1024.0/1024.0/1024.0*8, 4)) + ' Gbps')
+    print('Rx BW: ' + str(0.0 if bandwidth_rx < 200 else round(bandwidth_rx/1024.0/1024.0/1024.0*8, 4)) + ' Gbps')
 
-    last_index_total_tx1 = result[index_total_tx]
-    last_index_total_rx1 = result[index_total_rx]
+    last_tx1 = current_tx
+    last_rx1 = current_rx
 
-    interface = 'enp178s0f1'
-    baescmd = ("sudo  ethtool -S ") + interface
+    interface = 'enp37s0f1np1'
+    
+    current_tx = read_counter(interface, 'tx_bytes')
+    current_rx = read_counter(interface, 'rx_bytes')
 
-    result = subprocess.check_output(baescmd, shell=True).split()
-    index_total_tx = result.index('rx_bytes_phy:') + 1
-    index_total_rx = result.index('tx_bytes_phy:') + 1
-
-    if (not init):
-        bandwidth_tx = (float(result[index_total_tx]) - float(result[index_total_tx])) / interval
-        bandwidth_rx = (float(result[index_total_rx]) - float(result[index_total_rx])) / interval
+    if not init:
+        bandwidth_tx = (current_tx - current_tx) / interval
+        bandwidth_rx = (current_rx - current_rx) / interval
         init = True
     else:
-        bandwidth_tx = (float(result[index_total_tx]) - float(last_index_total_tx2)) / interval
-        bandwidth_rx = (float(result[index_total_rx]) - float(last_index_total_rx2)) / interval
+        bandwidth_tx = (current_tx - last_tx2) / interval
+        bandwidth_rx = (current_rx - last_rx2) / interval
 
     print(interface + ':')
-    # print('rx_bytes_phy: ' + str(result[index_total_tx]))
-    # print('tx_bytes_phy: ' + str(result[index_total_rx]))
-    print('Tx BW: ' + str(0.0 if bandwidth_tx < 200 else bandwidth_tx/1024.0/1024.0/1024.0*8) + ' Gbps')
-    print('Rx BW: ' + str(0.0 if bandwidth_rx < 200 else bandwidth_rx/1024.0/1024.0/1024.0*8) + ' Gbps')
-    last_index_total_tx2 = result[index_total_tx]
-    last_index_total_rx2 = result[index_total_rx]
+    print('Tx BW: ' + str(0.0 if bandwidth_tx < 200 else round(bandwidth_tx/1024.0/1024.0/1024.0*8, 4)) + ' Gbps')
+    print('Rx BW: ' + str(0.0 if bandwidth_rx < 200 else round(bandwidth_rx/1024.0/1024.0/1024.0*8, 4)) + ' Gbps')
+
+    last_tx2 = current_tx
+    last_rx2 = current_rx
 
     time.sleep(interval)
